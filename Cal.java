36e624f01f8fa655270cb3080574181c22b3c1bb
krishna <krishna4.b@gmail.com>
2026-02-18 12:58:50 +0000

    java-task

---
+++ Cal.java
@@ -0,0 +1,10 @@
+public class Cal{
+	public static void main(String[] args) {
+		int a=10;
+		int b=5;
+		int add=a+b;
+		int sub=a-b;
+		System.out.prinln("The sum is:"+add);
+		Systemc.out.println("The Difference is:"+sub);
+	}
+}
